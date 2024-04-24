8c3db6bda90e5eaec852ee078b5fa878c7fee860
navateja45 <navateja88011@gmail.com>
2024-04-24 21:51:34 +0530

    done

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+function connectDB(){
+
+    mongoose.connect('mongodb+srv://navateja:navateja88011@nodetuts.dw7vlvt.mongodb.net/sheycars' , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+
+}
+
+connectDB()
+
+module.exports = mongoose
